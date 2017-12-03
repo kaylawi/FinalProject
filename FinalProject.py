e8da3d848766ee76656dc4717e98b2d686dc8a42
Kayla <Kaylawi@0587345021.wireless.umich.net>
2017-12-03 15:46:18 -0500

    message

--- FinalProject.py
+++ FinalProject.py
@@ -1,2 +1,55 @@
-#Kayla Williams 
-#Final Project
+## SI 206 2017
+## Final Project
+
+##OBJECTIVE:
+##
+
+
+import unittest
+import itertools
+import collections
+import json
+ #import squlite3 
+import facebook
+
+##Your name: Kayla Williams
+
+
+##### FACEBOOK SETUP CODE:
+# Authentication information should be in a facebook_info file...
+
+
+# graph = facebook.GraphAPI(access_token = )
+
+
+##### END FACEBOOK SET UP CODE
+
+
+
+##### INSTAGRAM SETUP CODE:
+# Authentication information should be in a instagram_info file
+
+
+
+
+##### END INSTAGRAM SET UP CODE 
+
+
+
+
+
+##### PINTEREST SETUP CODE:
+# Authentication information should be in a pinterest_info file
+
+
+
+
+##### END PINTEREST SETUP CODE:
+
+
+#PINTEREST
+
+#GROUPME
+
+
+#GMAIL
